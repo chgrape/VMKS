8a74ec610fa019cc665ee630df2f9107387b612b
unknown <krisugergaming@gmail.com>
2023-06-09 13:06:37 +0300

    Edit

--- script.py
+++ script.py
@@ -1,13 +1,10 @@
 import serial
 
-# Define the serial port and baud rate
-port = 'COM5'  # Replace 'COMX' with the assigned COM port of your Arduino Bluetooth module
-baud_rate = 9600
+port = 'COM6'
+baud_rate = 19200
 
-# Create a serial object
 serial_port = serial.Serial(port, baud_rate, timeout=1)
 
-# Read the incoming data
 while True:
     data = serial_port.readline().decode().strip()
     if data:
